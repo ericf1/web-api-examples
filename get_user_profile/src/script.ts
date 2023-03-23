2e978babbdcb82db9291aa8bc456f34916bb3dc9
Eric <89803837+ericf1@users.noreply.github.com>
2023-03-23 17:22:53 -0500

    tailwind set up

--- get_user_profile/src/script.ts
+++ get_user_profile/src/script.ts
@@ -1,35 +1,49 @@
-// Because this is a literal single page application
-// we detect a callback from Spotify by checking for the hash fragment
-import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
+console.log("hello world")
 
-const clientId = "your_client_id";
-const params = new URLSearchParams(window.location.search);
-const code = params.get("code");
-
-if (!code) {
-    redirectToAuthCodeFlow(clientId);
-} else {
-    const accessToken = await getAccessToken(clientId, code);
-    const profile = await fetchProfile(accessToken);
-    populateUI(profile);
-}
-
-async function fetchProfile(code: string): Promise<UserProfile> {
-    const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${code}` }
+async function getSpotifyData() {
+    const token = "BQDCXKSjmisuqwcN1Zde6p5h5xBV1ficZCbaEwXPODNPJPAgJTwN_glQCJHrtufIqcjHebBXS1bI3za8lgDzieXx5yZErYL2iHKf5cXeLDhE7-pQLeDQrmgv2PCiVFAzU2UbNQYa_MIB0dc5s4nXuQm2sHLXX4u_htOd4HKEP9htrAObDsHLluE"
+    
+    const result = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=5", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
 
     return await result.json();
 }
 
-function populateUI(profile: UserProfile) {
-    document.getElementById("displayName")!.innerText = profile.display_name;
-    document.getElementById("avatar")!.setAttribute("src", profile.images[0].url)
-    document.getElementById("id")!.innerText = profile.id;
-    document.getElementById("email")!.innerText = profile.email;
-    document.getElementById("uri")!.innerText = profile.uri;
-    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url")!.innerText = profile.href;
-    document.getElementById("url")!.setAttribute("href", profile.href);
-    document.getElementById("imgUrl")!.innerText = profile.images[0].url;
+async function main() {
+    const data = await getSpotifyData()
+
+    data.items.forEach((item: any, index: number) => {
+        const img = document.createElement("img")
+        img.src = item.album.images[0].url
+        img.alt = item.name
+        img.className = "d-block w-100"
+        img.style.height = "300px"
+        img.style.objectFit = "cover"
+        img.style.objectPosition = "center"
+
+        const div = document.createElement("div")
+        div.className = "carousel-item flex flex-col-reverse"
+        div.appendChild(img)
+
+        const h5 = document.createElement("h5")
+        h5.innerHTML = item.name
+        h5.className = "mt-3"
+
+        const p = document.createElement("p")
+        p.innerHTML = item.artists[0].name
+
+        const div2 = document.createElement("div")
+        div2.className = "carousel-caption d-none d-md-block"
+        div2.appendChild(h5)
+        div2.appendChild(p)
+
+        div.appendChild(div2)
+        
+        document.getElementById("carousel-inner")?.appendChild(div)
+    })
 }
+
+main()
+
+export{}
